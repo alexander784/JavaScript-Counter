53f5707a28195c11a648f54d9ee6bd5cbf103238
alexander <alexandernyaga1999@gmail.com>
2023-11-24 16:14:21 +0300

    Add optionwrapper

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 const header = document.querySelector(".header");
 const main = document. querySelector(".main");
 
+//create a new varible "optionswrapper with a div"
+const optionsWrapper = document.createElement("div");
+//set the value of the attribute
+optionsWrapper.setAttribute("class", "optionsWrapper");
+//append the div to the header
+header.appendChild(optionsWrapper)
+
+
